9fd79ebc6bed77d7de4546ec792678a8d143c056
japa777 <@ejapa609gmail.com>
2021-02-12 04:43:05 -0400

    First commit

---
+++ japabot/Android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (darkbot3.0)
+ *
+ * Version information   (3.0)
+ *
+ * Date                  (06/12,15:35)
+ *
+ * author                (japa YT)
+ * Copyright notice      (bot com suporte a Android/Java)
